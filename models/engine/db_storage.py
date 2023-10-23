76298a0b6977ac92f5ee20728c6fbd0fc2b4189f
hillary-254 <hillarykim001@gmail.com>
2023-10-23 23:59:40 +0300

    7. Improve engines

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""This module defines a class to manage db storage for hbnb clone"""
+""" new class for sqlAlchemy """
 from os import getenv
 from sqlalchemy.orm import sessionmaker, scoped_session
 from sqlalchemy import (create_engine)
@@ -14,85 +14,7 @@ from models.amenity import Amenity
 
 
 class DBStorage:
-    """Defines the DBStorage class."""
-    # __engine = None
-    # __session = None
-
-    # def __init__(self):
-    #     """
-    #     Initializes the DBStorage class.
-    #     """
-    #     user = getenv("HBNB_MYSQL_USER")
-    #     passwd = getenv("HBNB_MYSQL_PWD")
-    #     db = getenv("HBNB_MYSQL_DB")
-    #     host = getenv("HBNB_MYSQL_HOST")
-    #     env = getenv("HBNB_ENV")
-
-    #     self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'
-    #                                   .format(user, passwd, host, db),
-    #                                   pool_pre_ping=True)
-
-    #     if getenv('HBNB_ENV') == 'test':
-    #         Base.metadata.drop_all(self.__engine)
-
-    # def all(self, cls=None):
-    #     """
-    #     Queries all objects of a certain class or all classes if no class is specified.
-    #     """
-    #     new_dict = {}
-    #     if cls:
-    #         objs = self.__session.query(cls).all()
-    #     else:
-    #         from models.state import State
-    #         from models.city import City
-    #         from models.user import User
-    #         from models.place import Place
-    #         from models.amenity import Amenity
-    #         from models.review import Review
-    #         classes = [State, City, User, Place, Amenity, Review]
-    #         objs = []
-    #         for cls in classes:
-    #             objs.extend(self.__session.query(cls).all())
-    #     for obj in objs:
-    #         key = "{}.{}".format(type(obj).__name__, obj.id)
-    #         new_dict[key] = obj
-    #     return new_dict
-
-    # def new(self, obj):
-    #     """
-    #     Adds an object to the current database session.
-    #     """
-    #     self.__session.add(obj)
-
-    # def save(self):
-    #     """
-    #     Commits all changes of the current database session.
-    #     """
-    #     self.__session.commit()
-
-    # def delete(self, obj=None):
-    #     """
-    #     Deletes an object from the current database session.
-    #     """
-    #     if obj:
-    #         self.__session.delete(obj)
-
-    # def reload(self):
-    #     """
-    #     Creates all tables in the database and creates the current database session from the engine.
-    #     """
-    #     Base.metadata.create_all(self.__engine)
-    #     session_factory = sessionmaker(bind=self.__engine,
-    #                                    expire_on_commit=False)
-    #     Session = scoped_session(session_factory)
-    #     self.__session = Session()
-
-    # def close(self):
-    #     """
-    #     Calls remove()
-    #     """
-    #     self.__session.close()
-
+    """ create tables in environmental"""
     __engine = None
     __session = None
 
--- models/state.py
+++ models/state.py
@@ -1,47 +1,30 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
+"""State Module for HBNB project"""
 from sqlalchemy.ext.declarative import declarative_base
 from models.base_model import BaseModel, Base
 from sqlalchemy.orm import relationship
-from sqlalchemy import Column, Integer, String
-import models
+from sqlalchemy import Column, Integer, String, ForeignKey
+from os import getenv
 from models.city import City
-import shlex
 
 
 class State(BaseModel, Base):
-    """ State class """
-    # __tablename__ = "states"
-    # name = Column(String(128), nullable=False)
-    # if getenv('HBNB_TYPE_STORAGE') == 'db':
-    #     cities = relationship("City", backref="state", cascade="all, delete")
-    # else:
-    #     @property
-    #     def cities(self):
-    #         """Get cities for a specific state"""
-    #         from models import storage
-    #         all_cities = storage.all(City)
-    #         state_cities = []
-    #         for city in all_cities.values():
-    #             if city.state_id == self.id:
-    #                 state_cities.append(city)
-    #         return state_cities
+    """State Module for HBNB project
+    Attributes:
+        name: input name
+    """
+
+    if getenv("HBNB_TYPE_STORAGE") != "db":
+        @property
+        def cities(self):
+            """
+            Get the list of City objects associated with this state
+            """
+            cities = models.storage.all(City)
+            return [city for city in cities.values()
+                    if city.state_id == self.id]
+
     __tablename__ = "states"
     name = Column(String(128), nullable=False)
     cities = relationship("City", cascade='all, delete, delete-orphan',
                           backref="state")
-
-    @property
-    def cities(self):
-        var = models.storage.all()
-        lista = []
-        result = []
-        for key in var:
-            city = key.replace('.', ' ')
-            city = shlex.split(city)
-            if (city[0] == 'City'):
-                lista.append(var[key])
-        for elem in lista:
-            if (elem.state_id == self.id):
-                result.append(elem)
-        return (result)
